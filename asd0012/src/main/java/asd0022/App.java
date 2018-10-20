3e77ebd408e2b58a1b09d316b8129bbf136aed1a
杨旋 <364105996@qq.com>
2018-10-20 15:36:59 +0800

    - init vscode java env

---
+++ asd0012/src/main/java/asd0022/App.java
@@ -0,0 +1,18 @@
+package asd0022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    
+    public static void test001() {
+        System.out.println("test001");
+    }
+}
---
+++ asd0012/src/test/java/asd0022/AppTest.java
@@ -0,0 +1,25 @@
+package asd0022;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testHello() {
+        System.out.println("--- testHello");
+    }
+}
